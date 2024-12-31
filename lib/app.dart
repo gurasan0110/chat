89e3cbbaac7097c26ba87f9fc242d4dfff93f0ab
gurasan0110 <gurasan0110@icloud.com>
2025-01-01 00:24:06 +0900

    HomePageを作成

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:chat/ui/widgets/pages/home_page/home_page.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
